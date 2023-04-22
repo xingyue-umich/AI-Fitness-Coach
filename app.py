2d10da661c83ad01d45579aa5d142ce0a4f2bad7
Sherry Zhang <56553466+xingyue-umich@users.noreply.github.com>
2023-04-22 01:37:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ import openai
 app = Flask(__name__)
 openai.api_key = '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'
 
-def create_plan_prompt(goal, days_per_week, hour_per_training, place, weight, age, gender, experience, height):
-    prompt = f"Create a weekly workout plan for the person who is {age} years old, {height} feet tall, and weighs {weight} lbs. \
+
+def create_plan_prompt(goal, days_per_week, hours_per_training, place, weight, age, gender, experience, height):
+    prompt = f"Create a weekly workout plan for a person who is {age} years old, {height} feet tall, and weighs {weight} lbs. \
     This person's gender is {gender} and their workout experience level is {experience}. \
-    Their primary goal is to {goal}, and they want to work out {days_per_week} days per week,\
-        each workout is around {hour_per_training} hours. They usually work out at {place}. Limit the plan to 8 actitivies per day."
+    Their primary goal is to {goal}, and they want to work out exactly {days_per_week} days per week, each workout is around {hours_per_training} hours. \
+    They usually work out at {place}. \
+    Limit the plan to 6 activities per day and try not to plan 3 consecutive workout days. \
+    Make sure to generate a plan with exactly {days_per_week} days of workout activities and the remaining days as rest days \
+    nMonday: \nTuesday: \nWednesday: \nThursday: \nFriday: \nSaturday: \nSunday: "
+
+    
     return prompt
 
 
@@ -35,7 +41,7 @@ def plan():
         response = openai.Completion.create(
             engine='text-davinci-003',
             prompt=prompt,
-            max_tokens=512,
+            max_tokens=1024,
             temperature=0.7
         )
         plan = response.choices[0].text.split("\n")
@@ -48,8 +54,8 @@ def plan():
         current_day = ''
         for line in plan:
             line = line.strip()
-            if line.startswith(('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')):
-                current_day = line
+            if line.startswith(tuple(days)):
+                current_day = line.split(':')[0].strip()
             elif line != "":
                 plan_dict[current_day].append(line)
 
